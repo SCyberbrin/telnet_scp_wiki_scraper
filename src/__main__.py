fb8840fcbdedd9944db9d3095dca1de0ed5b81d7
Silvano Stefani <silvstefani10@gmail.com>
2022-10-27 10:11:19 +0200

    added help message

--- src/__main__.py
+++ src/__main__.py
@@ -55,7 +55,7 @@ PERPETRATORS WILL BE TRACKED, LOCATED, AND DETAINED"""
 
     fake_login(conn)
 
-    conn.send("Type the nummber of the SCP your searching. \n\r Type 'info' for information about the client and 'quit' for disconnecting.".encode(UNICODE))
+    conn.send("Type the nummber of the SCP your searching. \n Type 'info' for information about the client and 'quit' for disconnecting.\n\r".encode(UNICODE))
         
     while True:
         try:
