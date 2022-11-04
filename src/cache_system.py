b095602c79d8359a087325f05752d30265c36b18
silvano <silvstefani10@gmail.com>
2022-11-04 21:19:51 +0100

    fixed temp folder

--- src/cache_system.py
+++ src/cache_system.py
@@ -1,15 +1,12 @@
 import platform, os, csv
 from typing import Union
 import logging
+import tempfile
+
+from src import UNICODE
 
 class scp_cache_system:
-    TMP_FOLDER = ""
-    if platform.system() == "Linux" or platform.system() == "Darwin":
-        TMP_FOLDER = "/tmp"
-    elif platform.system() == "Windows":
-        TMP_FOLDER = "C:\\TEMP"
-    
-    TMP_FILE = os.path.join(TMP_FOLDER, "scp_cache.tmp")
+    TMP_FILE = os.path.join(tempfile.gettempdir(), "scp_cache.tmp")
     
     def __init__(self) -> None:
         self.__create_exist()
@@ -22,7 +19,7 @@ class scp_cache_system:
 
     def add(self, scp_num: str, message: str) -> None:
         self.__create_exist()
-        with open(self.TMP_FILE, "a", newline="") as f_stream:
+        with open(self.TMP_FILE, "a", newline="", encoding=UNICODE) as f_stream:
             _writer = csv.writer(f_stream, delimiter=',',
                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
             _writer.writerow([scp_num, message])
@@ -37,7 +34,7 @@ class scp_cache_system:
 
     def __readLine(self, scp_num: str) -> Union[tuple[str, str], None]:
         self.__create_exist()
-        with open(self.TMP_FILE, newline="") as f_stream:
+        with open(self.TMP_FILE, newline="", encoding=UNICODE) as f_stream:
             _reader = csv.reader(f_stream, delimiter=',', quotechar='"')
 
             for data in _reader:
@@ -47,7 +44,8 @@ class scp_cache_system:
     
     def __create_exist(self) -> None:
         if not os.path.exists(self.TMP_FILE):
-            open(self.TMP_FILE, "x").close()
+            open(self.TMP_FILE, "x", encoding=UNICODE).close()
 
     def remove(self, scp_num: int) -> None:
+        # TODO: later
         pass
